4bcc59aeb9f536bfd9eda79787acafe006879fe4
jiajintao <jiajintao20210101@163.com>
2021-06-01 13:37:12 +0800

    修改添加文件template

--- testing_manage_platform/settings.py
+++ testing_manage_platform/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'testing_manage',
+    'report_manage'
 ]
 
 MIDDLEWARE = [
@@ -91,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'testing_manage_platform',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': 3306,
     }
